138f3c4212634ac9e11adb82f2707b756602a30f
Malihanaz09 <malihanaz541@gmail.com>
2025-07-07 12:29:30 +0500

    add_class_10

---
+++ class10_js/first.js
@@ -0,0 +1,29 @@
+console.log("Hello, World!");
+
+alert("Welcome to Class 10 of Web Development!");
+
+prompt("Please enter your name:");
+
+let a;
+a = 10;
+console.log(a);
+let A_1 = 20;
+console.log(A_1);
+
+const fullName = "Syed Zain Ali";
+let age = 15;
+console.log(fullName + " is " + age + " years old.");
+
+let isStudent = false;
+console.log("Is " + fullName + " a student? " + isStudent);
+
+let b = null;
+console.log("Value of b is: " + b);
+
+// Array Example
+let fruits = ["Apple", "Banana", "Cherry"];
+console.log(fruits);
+
+// Object Example
+let student = {name: "Zain", age: 15, rollNo: 12345};
+console.log(student);
